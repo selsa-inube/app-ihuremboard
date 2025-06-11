422593d3ef9283281532f9cab3be58bbb5ee7988
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-06-11 18:10:16 -0500

    fix: hook removal

--- src/ProtectedAppPage.tsx
+++ src/ProtectedAppPage.tsx
@@ -2,29 +2,17 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { AppPage } from "@components/layout/AppPage";
-
 import { useAppContext } from "@context/AppContext/useAppContext";
-import { useOptionsMenu } from "@hooks/useOptionsMenu";
 
 function ProtectedAppPage() {
-  const {
-    selectedClient,
-    provisionedPortal,
-    optionForCustomerPortal,
-    setOptionForCustomerPortal,
-  } = useAppContext();
+  const { selectedClient } = useAppContext();
   const navigate = useNavigate();
-
-  const publicCode = provisionedPortal.publicCode ?? "";
-  const clientId = selectedClient?.id ?? "";
-  const { optionData } = useOptionsMenu(publicCode, clientId);
-  setOptionForCustomerPortal(optionData);
 
   useEffect(() => {
     if (!selectedClient) {
       navigate("/login", { replace: true });
     }
-  }, [selectedClient, navigate, optionForCustomerPortal]);
+  }, [selectedClient, navigate]);
 
   return <AppPage />;
 }
