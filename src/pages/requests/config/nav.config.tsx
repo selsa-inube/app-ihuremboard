775dc8be48b83fb29d193a1489a528fb9dabfe5f
Angel David <125980817+Angelbt12@users.noreply.github.com>
2025-07-08 12:33:00 -0500

    at/ihp164/board-minors (#6)
    * feat: minor adjustments
    
    * fix: error compilation tsc
    
    * fix: name setting
    
    * fix: employee name deletion
    
    * fix: employee name deletion

--- src/pages/requests/config/nav.config.tsx
+++ src/pages/requests/config/nav.config.tsx
@@ -2,8 +2,5 @@ const breadcrumbs = {
   label: "Solicitudes en trámite",
   url: "/",
 };
-const RequestsNav: Record<string, { path: string }> = {
-  Vinculación: { path: "/requests" },
-};
 
-export { breadcrumbs, RequestsNav };
+export { breadcrumbs };
