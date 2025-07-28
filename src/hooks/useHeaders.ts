88f9682efffaf94607ba5f0f5bb95fc76e0da548
Angel David <125980817+Angelbt12@users.noreply.github.com>
2025-07-28 10:32:34 -0500

    at/ihp204/refresh-dashboard-data (#12)
    * feat: refresh dashboard data
    
    * fix: remove comment
    
    * fix: business unit adjustment
    
    * fix: console setting

--- src/hooks/useHeaders.ts
+++ src/hooks/useHeaders.ts
@@ -1,14 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useAppContext } from "@context/AppContext/useAppContext";
 
 export const useHeaders = () => {
   const { getAccessTokenSilently } = useAuth0();
+  const { selectedClient } = useAppContext();
 
   const getHeaders = async () => {
     const accessToken = await getAccessTokenSilently();
     return {
-      "Content-type": "application/json; charset=UTF-8",
-      "X-Business-unit": "test",
+      "Content-Type": "application/json; charset=UTF-8",
       Authorization: `Bearer ${accessToken}`,
+      "X-Business-Unit": selectedClient?.id ?? "",
     };
   };
 
